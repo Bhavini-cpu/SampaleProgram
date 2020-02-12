8382f71431dec746def4e2382b14fa1c805c9d81
bhavini <jainbhavini45@gmail.com>
2020-02-12 23:30:44 +0530

    Count max value from array

---
+++ SampleProgram/src/SampleProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleProgram
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(' ');
+            int[] arrElement = arr.Select(a => Convert.ToInt32(a)).ToArray();
+            int result = birthdayCakeCandles(arrElement);
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+        private int birthdayCakeCandles(int[] ar)
+        {
+            int maxVal = ar.Max();
+            return ar.Where(a => a == maxVal).Count();
+        }
+    }
+}
